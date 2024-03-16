822bfc48f8ee784a6c1798d85a5067e3b9ce3574
Zercopral <80348366+Zercopral@users.noreply.github.com>
2024-03-16 13:02:53 +0000

    CP1_Final

--- CP1_GN/CP1_GN.clj
+++ CP1_GN/CP1_GN.clj
@@ -1,33 +1,16 @@
 ;; (load-file "CP1_GN/CP1_GN.clj")
-
-(:require [clojure.math.numeric-tower :as math])
+;; (start 1 100)
+;; (guess-my-number)
+;; (smaller)
+;; (bigger)
 
 (def range_begin (atom 0))
 (def range_end (atom 0))
 (def guess (atom 0))
 
-(defn start
-  [begin end]
-  ;; (println (str(+ begin end)))
-  ;; (let [r_begin (Integer/parseInt begin), r_end (Integer/parseInt end)]
-  
-  ;; )
-  (swap! range_begin (fn [_] begin))
-  (swap! range_end (fn [_] end))
-  "Я готов...")
-
-(defn calc_guess [_]
-;;   (let [temp (- @range_end @range_begin)]
-;;     (if (odd? temp)
-;;       (- (+ @range_begin (/ (+ temp 1) 2) 1))
-;;       (+ @range_begin (/ temp 2)))
-;; ))
-(+ @range_begin (/ (round (- @range_end @range_begin)) 2))
-)
-
 (defn guess-my-number
   []
-  (swap! guess calc_guess);;(fn [left right] ) (@range_begin @range_end))
+  (swap! guess (fn [_] (+ @range_begin (int (/ (- @range_end @range_begin) 2)))))
   @guess)
 
 (defn smaller
@@ -39,3 +22,9 @@
   []
   (swap! range_begin (fn [_] @guess))
   (guess-my-number))
+
+(defn start
+  [begin end]
+  (swap! range_begin (fn [_] begin))
+  (swap! range_end (fn [_] end))
+  "Я готов...")
